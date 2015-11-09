bbf33a49e5d1d01b6f3cb2166a1b5b5e4a8caa0b
nicolas9101 <nicolas9101@yahoo.com>
2015-11-09 22:04:09 +0000

    Work done on the php.result, in progress -Sigrunn

--- results.php
+++ results.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $host = "127.0.0.1";
 $user = "root";                    
@@ -13,91 +12,7 @@ if(empty($aGenre))
 else
 {
      $N = count($aGenre);
-     
-     echo("You selected $N genre(s): ");
-     for($i=0; $i < $N; $i++)
-     {
-          echo($aGenre[$i] . " ");
-     }
 }
-
-
-/*
-foreach($_POST['genre'] as $genre)
-{
-  echo $genre;
-     
-}
-*/
-
-//title trailer poster rating description
-$conn = mysqli_connect($host, $user, $pass, $db) or die(mysql_error());
-
-$sql = "SELECT title FROM films";
-
-$result = mysqli_query($conn, $sql);
-
-//while ($row = $result->fetch_assoc()) {
-//	foreach ($row as $title) {
-		//echo $title . "\n";
-//	}
-//}
-?>
-
-=======
->>>>>>> ab13cf1585a025d2cf967b23c59c489714bced46
-<html>
-<style type="text/css">
-     body{
-          background-color: #141414;
-          margin:0;
-          padding:0;
-          height:100%;
-     }
-     h1{
-          color: red;
-          font-family: arial black;
-          display: block;
-          font-size: 40px;
-          padding-left: 20px;
-          padding-right: 10px;
-          perspective: 100px;
-          perspective-origin: 50% 0;
-          transform-origin: 0 0;
-          transform: scaleX(80) rotateY(89.5deg);
-      }
-      p{
-          color: red;
-          font-size: 20px;
-          display: block;
-          font-family: arial black;
-      }
-  
-  </style>
-<body>
-
-<?php
-$host = "127.0.0.1";
-$user = "root";                    
-$pass = "";                                 
-$db = "Netflix"; 
-
-$aGenre = $_POST['genre'];
-if(empty($aGenre))
-{
-     echo("You didn't select any genres.");
-}
-else
-{
-     $N = count($aGenre);
-     
-#     echo("You selected $N genres(s): ");
-#     for($i=0; $i < $N; $i++)
-#     {
-#          echo($aGenre[$i] . " ");
-#     }
-}
-
 //title trailer poster rating description
 $conn = mysqli_connect($host, $user, $pass, $db) or die(mysql_error());
 
@@ -119,20 +34,86 @@ for($i=0; $i < $N; $i++)
      $film_details = mysqli_query($conn, $sql);
      mysqli_data_seek($film_details, 0);
      $row = mysqli_fetch_assoc($film_details);
+     $title = $row['title'];
      $trailer = $row['trailer'];
      $poster = $row['poster'];
      $rating = $row['rating'];
      $description = $row['description'];
 ?>
+
+<html>
+<style type="text/css">
+     body{
+          background-color: #141414;
+          margin:0;
+          padding:0;
+          height:100%;
+     }
+     head{
+          color:white;
+          font-size: 14px;
+          display: block;
+          font-family: arial black;
+          padding-left: 20px;
+          padding-right: 10px;
+     }
+     h1{
+          margin-left: 400 px;
+          color: red;
+          font-family: arial black;
+          display: block;
+          font-size: 40px;
+          padding-left: 20px;
+          padding-right: 10px;
+          perspective: 100px;
+          perspective-origin: 50% 0;
+          transform-origin: 0 0;
+          transform: scaleX(80) rotateY(89.5deg);
+      }
+     p{
+          color: white;
+          font-size: 20px;
+          display: block;
+          font-family: arial black;
+      }
+      description{
+          color:white;
+          font-size: 12px;
+          display: block;
+          font-family: arial;
+          margin-right: 200px;
+          position: relative;
+      float: right;
+
+      }
+      poster{
+          padding-left: 20px;
+          padding-right: 10px;
+          padding-bottom: 5px;
+          padding-top:20px;
+      }
+      rating{
+          color:white;
+          font-size: 14px;
+          display: block;
+          font-family: arial black;
+          position: relative;
+          float: right;
+      }
+    </style>
+<body>
+     <head>
+       <?php echo $genre ?>
+     </head>
+     <h1><?php echo $title?></h1>
+    
+     <poster><img src="<?php echo $poster?>" alt="<?php echo $title?> poster" height="300"></poster>
+     <rating>IMDB rating: <?php echo $rating?>/10</rating>
+     <description><?php echo $description?></description>
+     <iframe width="420" height="315"
+     src="<?php echo $trailer?>">
+     </iframe>
      
-
-    <h1><?php echo $genre ?></h1>
-    <p><?php echo $trailer?></p>
-    <p><?php echo $poster?></p>
-    <p><?php echo $rating?></p>
-    <p><?php echo $description?></p>
-
-
 <?php
 }
 ?>
